b96f2928d4ce85d09c34610d1882bce39603a57c
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-07-16 13:16:14 +0900

    scanner

--- Mobile/Hackathon/app/src/main/java/com/example/hackathon/activity/MainActivity.java
+++ Mobile/Hackathon/app/src/main/java/com/example/hackathon/activity/MainActivity.java
@@ -1,4 +1,49 @@
 package com.example.hackathon.activity;
 
-public class MainActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.example.hackathon.R;
+import com.example.hackathon.databinding.MainActivityBinding;
+import com.example.hackathon.viewmodel.MainViewModel;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends BaseActivity<MainActivityBinding, MainViewModel> {
+
+    @Override
+    protected int layoutId() {
+        return R.layout.main_activity;
+    }
+
+    @Override
+    protected Class viewModel() {
+        return MainViewModel.class;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new IntentIntegrator(this).initiateScan();
+        clickEvent();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+
+        Toast.makeText(this,result.getContents(),Toast.LENGTH_LONG).show();
+    }
+
+    private void clickEvent() {
+
+//        binding.camera.setOnClickListener(v -> );
+    }
 }
---
+++ Mobile/Hackathon/app/src/main/java/com/example/hackathon/viewmodel/MainViewModel.java
@@ -0,0 +1,10 @@
+package com.example.hackathon.viewmodel;
+
+import android.content.Context;
+
+public class MainViewModel extends BaseViewModel {
+
+    public MainViewModel(Context context) {
+        super(context);
+    }
+}
