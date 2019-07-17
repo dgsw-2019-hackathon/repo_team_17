743d0aff88a8d9a5610bac31163dc22f2f17dd5d
rjsdnqkr1 <39177603+rjsdnqkr1@users.noreply.github.com>
2019-07-17 15:26:31 +0900

    321321

--- Mobile/Hackathon/app/src/main/java/com/example/hackathon/Constants.java
+++ Mobile/Hackathon/app/src/main/java/com/example/hackathon/Constants.java
@@ -3,22 +3,6 @@ package com.example.hackathon;
 import android.os.Environment;
 
 public final class Constants {
-
-    public static final String DIRECTORY_DOWNLOADS = Environment.DIRECTORY_DOWNLOADS + "/DodamDodam";
-
-    public static final String SOCKET_HOST = "http://192.168.0.55:3000";
     public static final String DEFAULT_HOST = "http://192.168.137.1:8888";
-
-    public static final String TEST_HOST = "http://10.80.162.124:8000";
-
-        /*
-         광용 192.168.0.20
-         태형 192.168.0.18 (10.80.161.100)
-         진영 192.168.0.19
-         서연 192.168.0.42
-         10.80.162.124
-         M2SYS HOST 192.168.0.55
-         HOST 49.247.130.189:35800
-        */
 
 }
--- Mobile/Hackathon/app/src/main/java/com/example/hackathon/Utils.java
+++ Mobile/Hackathon/app/src/main/java/com/example/hackathon/Utils.java
@@ -29,29 +29,4 @@ public final class Utils {
             builder.addInterceptor(new HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY));
         return builder.build();
     }
-
-    public static String encryptSHA512(String target) throws NoSuchAlgorithmException {
-
-        final MessageDigest messageDigest = MessageDigest.getInstance("SHA-512");
-        final StringBuilder encryptedPassword = new StringBuilder();
-
-        messageDigest.update(target.getBytes());
-
-        final byte[] buffer = messageDigest.digest();
-
-        for (byte temp : buffer) {
-
-            StringBuilder sb = new StringBuilder(Integer.toHexString(temp));
-
-            while (sb.length() < 2) {
-
-                sb.insert(0, "0");
-            }
-
-            sb = new StringBuilder(sb.substring(sb.length() - 2));
-            encryptedPassword.append(sb);
-        }
-
-        return encryptedPassword.toString();
-    }
 }
